63c3d9d8ba6cbb82169ad2aed0ee6a688709bde3
Kynhoff <rkynhoff@gmail.com>
2024-10-24 22:58:20 -0400

    my version of FizzBuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,12 @@
 # add your code here
+for i in range(1,101):
+  if i % 3 == 0 and i % 5 == 0:
+    print("FizzBuzz")
+  elif i % 3 == 0 and i % 5 != 0:
+    print("Fizz")
+  elif i % 3 != 0 and i % 5 == 0:
+    print("Buzz")
+  else:
+    print(i)
 
+#I'm having difficulty with the instructions for Automated code testing on the ReadMe file. :(
